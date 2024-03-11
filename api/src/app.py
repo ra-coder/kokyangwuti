c950e99a272e69d4cabea8ecb9ec037776839b6a
ra-coder <d-sun-d@yandex.ru>
2024-03-11 03:07:04 +0300

    Add load file to s3 logic (#2)
    * Add endpoint for save input file to s3
    
    * Fix invalid redeploy
    
    * Fix invalid script
    
    * Repove problem part
    
    ---------
    
    Co-authored-by: aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>

--- api/src/app.py
+++ api/src/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
+from api import router
+
 app = FastAPI(root_path="/api")
+app.include_router(router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=8000,
+    )
